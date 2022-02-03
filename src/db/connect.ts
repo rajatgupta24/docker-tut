a79f51e7cae9eaeb7604563dd8dd4e7d8cfdeaba
rajat <rajat2411gupta@gmail.com>
2022-02-03 20:37:58 +0530

    Setting up node-server with typescript

---
+++ src/db/connect.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from "config";
+import log from "../logger";
+
+const connect = () => {
+    const dbUri = config.get("dbUri") as string;
+
+    return mongoose.connect(dbUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        log.info("DB connected");
+    }).catch((err) => {
+        log.info("ERROR", err)
+        process.exit(1);
+    })
+}
+
+export default connect;
